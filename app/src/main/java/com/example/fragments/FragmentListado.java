ebace61c3b7ad09b57dd982f5e67e95a8ef55d48
Raultxo <raul6114@gmail.com>
2021-11-03 09:50:23 +0100

    asd

---
+++ app/src/main/java/com/example/fragments/FragmentListado.java
@@ -0,0 +1,14 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentListado extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
