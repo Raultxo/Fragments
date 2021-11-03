db64a27138eae448a8e55cd276c5e396bd9204a0
Raultxo <raul6114@gmail.com>
2021-11-03 10:01:23 +0100

    aaa

---
+++ app/src/main/java/com/example/fragments/JuegoListener.java
@@ -0,0 +1,5 @@
+package com.example.fragments;
+
+public interface JuegoListener {
+    void onJuegoSeleccionado(Juego j);
+}
--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JuegoListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Prueba
+
+        FragmentListado fragmentListado = (FragmentListado) getSupportFragmentManager().findFragmentById(R.id.frgListado);
+
+    }
+
+    @Override
+    public void onJuegoSeleccionado(Juego j) {
+
     }
 }
